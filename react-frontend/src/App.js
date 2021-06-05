e04272276a9befe8a5ca1fec43f03faacdbb00d7
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-05 16:56:30 +0300

    create product component and display name, image, reviews and price on home screen

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
 
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to eShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
