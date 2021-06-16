ab52208875d454c3ab57088b76032dfe6e0a3070
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-17 00:02:00 +0300

    serialize product data and send to frontend

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from .products import products
+from rest_framework import serializers
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+
+from .models import Product
+from .products import products
+from .serializers import ProductSerializer
 
 
 @api_view(['GET', ])
@@ -13,7 +18,9 @@ def getRoutes(request):
 
 @api_view(['GET', ])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
 @api_view(['GET', ])
