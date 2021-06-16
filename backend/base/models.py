ab0149368f9ae75977342becb131816e921d9a15
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-16 16:40:11 +0300

    create shipping-address model

--- backend/base/models.py
+++ backend/base/models.py
@@ -68,3 +68,18 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+class ShippingAddress(models.Model):
+    order = models.OneToOneField(
+        Order, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    county = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.address)
