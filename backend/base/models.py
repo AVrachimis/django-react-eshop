12d29ad9f17320360dc2991b69f04d88a6ef77e8
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-16 16:19:07 +0300

    create review model

--- backend/base/models.py
+++ backend/base/models.py
@@ -21,3 +21,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.rating)
