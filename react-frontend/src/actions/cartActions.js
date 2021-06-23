1c7f7d9e311bcd372e706134c13315345dbb459d
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-23 09:51:54 +0300

    fix duplicate product on cart bug #5

--- react-frontend/src/actions/cartActions.js
+++ react-frontend/src/actions/cartActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
+import {
+    CART_ADD_ITEM,
+    CART_REMOVE_ITEM
+} from '../constants/cartConstants'
+
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
     const { data } = await axios.get(`/api/products/${id}`)
@@ -15,7 +19,6 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             qty
         }
     })
-
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
 
