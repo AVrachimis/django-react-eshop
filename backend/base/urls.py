aa65d0a442ebddcb1061097a29a15f2969dc5d7e
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-24 18:39:26 +0300

    add username and email to JWT token for easier authentication on frontend #9

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product'),
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
