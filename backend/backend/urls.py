9305434d9a9727971fe6ce3e5fa031c706502303
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-24 18:18:23 +0300

    install and setup json web tokens #9

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 urlpatterns = [
     path('', views.getRoutes, name='routes'),
     path('products/', views.getProducts, name='products'),
     path('products/<str:pk>/', views.getProduct, name='product'),
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 
 ]
