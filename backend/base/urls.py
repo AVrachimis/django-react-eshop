a656d5138e10855ed74169bca57d3d6cdb88e672
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-29 17:19:06 +0300

    Create users page for admins and add permission classes  #10

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('products/<str:pk>/', views.getProduct, name='product'),
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('users/profile/', views.getUserProfile, name='user-profile'),
+    path('users/', views.getUsers, name='users'),
 
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -3,9 +3,10 @@ from django.http import JsonResponse
 from rest_framework import serializers
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
-from .models import Product
+from .models import Product, User
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
@@ -25,7 +26,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
         for key,value in serializer.items():
             data[key] = value
 
-
+ 
         return data
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -41,10 +42,21 @@ def getRoutes(request):
 
 
 @api_view(['GET', ])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
+
+
+@api_view(['GET', ])
+@permission_classes([IsAdminUser])
+def getUsers(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
 
 
 @api_view(['GET', ])
