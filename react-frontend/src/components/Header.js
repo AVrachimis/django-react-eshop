11c9b5ce78df4c6dc66ea04101f3a0a2c0ab524f
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-06-30 17:17:34 +0300

    implement logout functionality #13

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Nav, Navbar, Container, Row, NavDropdown, } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
-
+import { logout } from '../actions/userActions'
 function Header() {
 
 
@@ -11,9 +11,10 @@ function Header() {
 
     const { userInfo } = userLogin
 
+    const dispatch = useDispatch()
+
     const logoutHandler = () => {
-
-
+        dispatch(logout())
     }
 
     return (
