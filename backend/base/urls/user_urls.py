16b4888f6b205aacece082d162e3de69bed69c45
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-02 16:05:04 +0300

    Continue user profile functionality-backend #15

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
 
     path('profile/', views.getUserProfile, name='user-profile'),
+    path('profile/update', views.updateUserProfile, name='user-profile-update'),
+
     path('', views.getUsers, name='users'),
 
 ]
