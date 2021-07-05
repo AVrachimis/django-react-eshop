d94a0d5e3d3fea4c094497fbb5587e01aa79dbb4
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-05 18:17:39 +0300

    initialize shipping screen and details form #16

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 
 
 function App() {
@@ -29,6 +30,7 @@ function App() {
 
           <Route path='/profile' component={ProfileScreen} />
 
+          <Route path='/shipping' component={ShippingScreen} />
 
         </Container>
       </main>
