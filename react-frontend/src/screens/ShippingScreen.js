4de93fbe4b1028fd2ad3904350d71367fedcbc9a
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-09 18:18:10 +0300

    create checkout steps component and add it to shipping page #24

--- react-frontend/src/screens/ShippingScreen.js
+++ react-frontend/src/screens/ShippingScreen.js
@@ -4,6 +4,7 @@ import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 import { saveShippingAddress } from '../actions/cartActions'
 
@@ -28,6 +29,8 @@ function ShippingScreen({ history }) {
 
     return (
         <FormContainer>
+
+            <CheckoutSteps step1 step2 />
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
 
