92879f3f1323fd62af0ba4143c7c12b636585e81
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-10 09:53:17 +0300

    display shipping and payment details on placeorderscreen #22

--- react-frontend/src/screens/PlaceOrderScreen.js
+++ react-frontend/src/screens/PlaceOrderScreen.js
@@ -11,6 +11,40 @@ function PlaceOrderScreen() {
     return (
         <div>
             <CheckoutSteps step1 step2 step3 step4 />
+
+            <Row>
+                <Col md={8}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h2>Shipping</h2>
+                            <p>
+                                <strong>Shipping: </strong>
+                                {cart.shippingAddress.address},{cart.shippingAddress.city},
+                                {'  '}
+                                {cart.shippingAddress.postalCode},
+                                {'  '}
+                                {cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Payment Method</h2>
+                            <p>
+                                <strong>Shipping: </strong>
+                                {cart.paymentMethod},{cart.shippingAddress.city},
+                                {'  '}
+                                {cart.shippingAddress.postalCode},
+                                {'  '}
+                                {cart.shippingAddress.country}
+                            </p>
+                        </ListGroup.Item>
+
+                    </ListGroup>
+                </Col>
+
+                <Col md={4}>
+                </Col>
+            </Row>
         </div>
     )
 }
