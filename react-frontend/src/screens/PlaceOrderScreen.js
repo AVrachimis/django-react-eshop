669449a448b4f46f9e5c4db62bc4f71c0be53b19
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-10 15:09:38 +0300

    display order items on placeorder screen #22

--- react-frontend/src/screens/PlaceOrderScreen.js
+++ react-frontend/src/screens/PlaceOrderScreen.js
@@ -30,13 +30,40 @@ function PlaceOrderScreen() {
                         <ListGroup.Item>
                             <h2>Payment Method</h2>
                             <p>
-                                <strong>Shipping: </strong>
-                                {cart.paymentMethod},{cart.shippingAddress.city},
-                                {'  '}
-                                {cart.shippingAddress.postalCode},
-                                {'  '}
-                                {cart.shippingAddress.country}
+                                <strong>Method: </strong>
+                                {cart.paymentMethod}
                             </p>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <h2>Order Items</h2>
+
+                            {cart.cartItems.length === 0 ?
+                                'Your Cart is Empty' : (
+                                    <ListGroup variant='flush'>
+                                        {cart.cartItems.map((item, index) => (
+                                            <ListGroup.Item key={index}>
+                                                <Row>
+                                                    <Col md={1}>
+                                                        <Image src={item.image} alt={item.name} fluid rounded />
+                                                    </Col>
+                                                    <Col>
+                                                        <Link to={`/product/${item.product}`}>{item.name}</Link>
+                                                    </Col>
+
+                                                    <Col md={4}>
+                                                        {item.qty} X ${item.price} = ${(item.qty * item.price).toFixed(2)}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                        ))}
+
+
+                                    </ListGroup>
+                                )
+
+                            }
+
                         </ListGroup.Item>
 
                     </ListGroup>
