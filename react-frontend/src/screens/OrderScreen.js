227669c4cb3a44e6168744e5ea835d93de17eac8
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-14 14:32:36 +0300

    add PayPal Payment buttons when order is not paid #29

--- react-frontend/src/screens/OrderScreen.js
+++ react-frontend/src/screens/OrderScreen.js
@@ -5,30 +5,67 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import FormContainer from '../components/FormContainer'
 
-import { getOrderDetails } from '../actions/orderActions'
+import { getOrderDetails, payOrder } from '../actions/orderActions'
+import { ORDER_PAY_RESET } from '../constants/orderConstants'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
+
+import { PayPalButton } from 'react-paypal-button-v2'
+
+
+
 
 function OrderScreen({ match }) {
 
     const orderId = match.params.id
     const dispatch = useDispatch()
 
+    const [sdkReady, setSdkReady] = useState(false)
+
 
     const orderDetails = useSelector(state => state.orderDetails)
     const { order, error, loading } = orderDetails
+
+
+    const orderPay = useSelector(state => state.orderPay)
+    const { loading: loadingPay, success: successPay } = orderPay
+
 
     if (!loading && !error) {
         order.itemsPrice = order.orderItems.reduce((acc, item) => acc + item.price * item.qty, 0).toFixed(2)
     }
 
+    const appPayPalScript = () => {
+        const script = document.createElement('script')
+        script.type = 'text/javascript'
+        script.src = 'https://www.paypal.com/sdk/js?client-id=ARyLA3qBNTRwecRJs3F5fqnALp6LMPDvQNRWGmDtSag8QBJzWv2w9CxScN2cYcWakYNElm3YdkC5a3JB'
+        script.async = true
+        script.onload = () => {
+            setSdkReady(true)
+        }
+        document.body.appendChild(script)
+    }
+
 
     useEffect(() => {
 
-        if (!order || order._id !== Number(orderId)) {
+        if (!order || successPay || order._id !== Number(orderId)) {
+            dispatch({ type: ORDER_PAY_RESET })
             dispatch(getOrderDetails(orderId))
+        } else if (!order.isPaid) {
+            if (!window.paypal) {
+                appPayPalScript()
+            } else {
+                setSdkReady(true)
+            }
+
         }
     }, [dispatch, order, orderId])
+
+
+    const successPaymentHandler = (paymentResult) => {
+        dispatch(payOrder(orderId, paymentResult))
+    }
 
 
     return loading ? (
@@ -146,6 +183,26 @@ function OrderScreen({ match }) {
                                     <Col>${order.totalPrice}</Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {!order.isPaid && (
+                                <ListGroup.Item>
+                                    {loadingPay && <Loader />}
+
+                                    {!sdkReady ? (
+                                        <Loader />
+                                    ) : (
+                                        <PayPalButton
+                                            amount={order.totalPrice}
+                                            onSuccess={successPaymentHandler}
+
+                                        />
+
+
+                                    )}
+
+                                </ListGroup.Item>
+
+                            )}
 
                         </ListGroup>
                     </Card>
