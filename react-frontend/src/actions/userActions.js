1b60888b37e4b2e6c5e4f488fb1ed47957be6648
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-15 19:28:28 +0300

    Create myOrders actions #27

--- react-frontend/src/actions/userActions.js
+++ react-frontend/src/actions/userActions.js
@@ -22,6 +22,8 @@ import {
     USER_LOGOUT,
 } from '../constants/userConstants'
 
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
+
 export const login = (email, password) => async (dispatch) => {
     try {
         dispatch({
@@ -63,6 +65,8 @@ export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({ type: USER_LOGOUT })
     dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
+
 
 }
 
