11d8c986e8999b14aa6705afa8bd16177ecf6617
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-15 19:22:57 +0300

    Create MyOrders reducers #27

--- react-frontend/src/store.js
+++ react-frontend/src/store.js
@@ -14,6 +14,7 @@ import {
     orderCreateReducer,
     orderDetailsReducer,
     orderPayReducer,
+    orderListMyReducer,
 } from './reducers/orderReducers'
 
 const reducer = combineReducers({
@@ -30,6 +31,8 @@ const reducer = combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
+
 
 })
 
