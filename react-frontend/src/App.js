eff8762f1d4ed6e1074300ffcfd466e4d02c3acf
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-16 13:36:12 +0300

    Created user edit screen from admin #32

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -14,6 +14,7 @@ import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 
 function App() {
@@ -44,6 +45,7 @@ function App() {
 
           <Route path='/admin/userList' component={UserListScreen} />
 
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
 
 
         </Container>
