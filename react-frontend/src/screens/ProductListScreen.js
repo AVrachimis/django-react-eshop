56d9462d42f48366b9698cb6e7c4c4ee07029357
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-19 12:17:44 +0300

    Finalize Create product button and functionality #35

--- react-frontend/src/screens/ProductListScreen.js
+++ react-frontend/src/screens/ProductListScreen.js
@@ -6,8 +6,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 
-import { listProducts, deleteProduct } from '../actions/productActions'
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions'
 
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants'
 
 function ProductListScreen({ history, match }) {
 
@@ -19,18 +20,28 @@ function ProductListScreen({ history, match }) {
     const productDelete = useSelector(state => state.productDelete)
     const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
 
+    const productCreate = useSelector(state => state.productCreate)
+    const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate
+
 
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
 
     useEffect(() => {
-        if (userInfo && userInfo.isAdmin) {
-            dispatch(listProducts())
-        } else {
+        dispatch({ type: PRODUCT_CREATE_RESET })
+
+        if (!userInfo.isAdmin) {
             history.push('/login')
         }
-    }, [dispatch, history, userInfo, successDelete])
+
+        if (successCreate) {
+            history.push(`/admin/product/${createdProduct._id}/edit`)
+        } else {
+            dispatch(listProducts())
+        }
+
+    }, [dispatch, history, userInfo, successDelete, successCreate, createProduct])
 
     const deleteHandler = (id) => {
         if (window.confirm("Are you sure you want to delete this product?")) {
@@ -39,8 +50,8 @@ function ProductListScreen({ history, match }) {
         }
     }
 
-    const createProductHandler = (product) => {
-        // create product
+    const createProductHandler = () => {
+        dispatch(createProduct())
     }
 
     return (
@@ -62,6 +73,9 @@ function ProductListScreen({ history, match }) {
             {loadingDelete && <Loader />}
             {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
             {successDelete && <Message variant='success'>Product Succesfully Deleted</Message>}
+
+            {loadingCreate && <Loader />}
+            {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
 
             {loading ?
                 <Loader />
