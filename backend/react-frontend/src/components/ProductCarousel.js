0945c31575d5f01a4d54763075a5e9e329a2b35c
Andreas Vrachimis <andreas.vr@hotmail.com>
2021-07-28 13:59:21 +0300

    Database switched to PosgreSQL #44

--- backend/react-frontend/src/components/ProductCarousel.js
+++ backend/react-frontend/src/components/ProductCarousel.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Carousel, Image } from 'react-bootstrap'
+import { Carousel, Image, Row, Col } from 'react-bootstrap'
 import Loader from './Loader'
 import Message from './Message'
 import { listTopProducts } from '../actions/productActions'
@@ -22,18 +22,26 @@ function ProductCarousel() {
         loading ? <Loader />
             : error ? <Message variant='danger'>{error}</Message>
                 : (
-                    <Carousel pause='hover' className='bg-dark'>
-                        {products.map(product => (
-                            <Carousel.Item key={product._id}>
-                                <Link to={`/product/${product._id}`}>
-                                    <Image src={product.image} alt={product.name} fluid />
-                                    <Carousel.Caption className='carousel.caption'>
-                                        <h4>{product.name} (${product.price})</h4>
-                                    </Carousel.Caption>
-                                </Link>
-                            </Carousel.Item>
-                        ))}
-                    </Carousel>
+                    <Row>
+                        <Col
+                            xs={5}
+                            className='mx-auto'
+                        >
+                            <Carousel pause='hover'>
+                                {products.map(product => (
+                                    <Carousel.Item key={product._id}>
+                                        <Link to={`/product/${product._id}`}>
+                                            <Image src={product.image} alt={product.name} fluid className='center' />
+                                            <Carousel.Caption className='carousel.caption'>
+                                                <h4>{product.name} (${product.price})</h4>
+                                            </Carousel.Caption>
+                                        </Link>
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
+
+                        </Col>
+                    </Row >
                 )
 
     )
